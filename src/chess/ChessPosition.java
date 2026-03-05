e0f652b9efa3ab11107d04d353bb28d5353269d1
lorranz <lorranz.lucena.sousa@gmail.com>
2026-03-05 14:24:37 -0300

    little improvemnte in board printin

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -6,6 +6,8 @@ public class ChessPosition {
 
 	private char column;
 	private int row;
+	
+	
 	public ChessPosition(char column, int row) {
 	
 		if(column < 'a' || column > 'h'|| row <1 || row >8 ) {
@@ -14,6 +16,9 @@ public class ChessPosition {
 		this.column = column;
 		this.row = row;
 	}
+	
+	
+	
 	public char getColumn() {
 		return column;
 	}
